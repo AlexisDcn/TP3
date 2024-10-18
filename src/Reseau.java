932a3e1a54a014ded1987549c295d3c34081a58f
Alexis Dechenais <alexis.dechenais@etud.univ-jfc.fr>
2024-10-18 09:58:17 +0200

    commit avant de faire la fonction calculer sortie de Reseau

--- src/Reseau.java
+++ src/Reseau.java
@@ -1,3 +1,19 @@
+import java.util.ArrayList;
+
 public class Reseau {
+
+Couche couche1 = new Couche(2);
+Couche couche2 = new Couche(1);
+
+ArrayList<Float> inputs = new ArrayList<Float>();
+
+public float calculerSortie(ArrayList<Float> inputs) {  //les poids en entrée
+    if (inputs.size() != 4) {
+        throw new IllegalArgumentException("erreur d'entrée.");
+    }
+
+    
+}
+
 
 }

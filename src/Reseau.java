6b5eecf1330b1b5d4f76cf657fd5d02d6681ad2d
Alexis Dechenais <alexis.dechenais@etud.univ-jfc.fr>
2024-10-18 10:23:20 +0200

    TP3 final (j'ai fait chat gpt pour la fct calculerSortie de la classe réseau)

--- src/Reseau.java
+++ src/Reseau.java
@@ -7,10 +7,28 @@ Couche couche2 = new Couche(1);
 
 ArrayList<Float> inputs = new ArrayList<Float>();
 
-public float calculerSortie(ArrayList<Float> inputs) {  //les poids en entrée
+public float calculerSortie(ArrayList<Float> inputs) {  //les entrées
     if (inputs.size() != 4) {
         throw new IllegalArgumentException("erreur d'entrée.");
     }
+    // Create inputs for the first layer
+    ArrayList<Float> inputsNeurone1 = new ArrayList<>();
+    inputsNeurone1.add(inputs.get(0)); // e1
+    inputsNeurone1.add(inputs.get(1)); // e2
+
+    ArrayList<Float> inputsNeurone2 = new ArrayList<>();
+    inputsNeurone2.add(inputs.get(2)); // e3
+    inputsNeurone2.add(inputs.get(3)); // e4
+
+    // Compute outputs of first layer
+    ArrayList<Float> outputsLayer1 = new ArrayList<>();
+    outputsLayer1.add((float) couche1.neurones.get(0).calculerSortie(inputsNeurone1)); // Neurone 1
+    outputsLayer1.add((float) couche1.neurones.get(1).calculerSortie(inputsNeurone2)); // Neurone 2
+
+    // Compute output of second layer (Neurone 3)
+    float finalOutput = couche2.neurones.get(0).calculerSortie(outputsLayer1);
+
+    return finalOutput;
 
     
 }

ec7f9d2fb93b26188e1d7bae0f623f2e1352bb45
Alexis Dechenais <alexis.dechenais@etud.univ-jfc.fr>
2024-10-18 09:43:32 +0200

    Classe couche

--- src/Couche.java
+++ src/Couche.java
@@ -9,8 +9,26 @@ public class Couche {
         neurones = new ArrayList<>(n);
         // Créer n neurones, chaque neurone ayant un nombre d'entrées défini
         for (int i = 0; i < n; i++) {
-            neurones.add(new Neurone(2));  // Chaque neurone aura un nombre d'entrées spécifié
+            neurones.add(new Neurone(2));  // Chaque neurone aura un nombre d'entrées = 2
         }
+    }
+
+    // Calculer la sortie de la couche
+    public int calculerSortie(ArrayList<Float> inputs) {
+        int sum = 0;
+        for (Neurone neurone : neurones) {
+            sum += neurone.calculerSortie(inputs);
+        }
+        return sum;
+    }
+
+    @Override
+    public String toString() {
+        String result = "couche : \n";
+        for (Neurone neurone : neurones) {
+            result += neurone.toString() + "\n";
+        }
+        return result;
     }
 
 }
